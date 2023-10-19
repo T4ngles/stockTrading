ea5b86559f282ee9d597c69a23a64757114a4e1e
T4ngles <rlau06@gmail.com>
2023-10-19 15:45:26 +1100

    Live Ticker - adjust ex date formatting and include legend details on div and ex dates

--- Live_Ticker.py
+++ Live_Ticker.py
@@ -14,18 +14,20 @@ class Stock_Trace:
     
     def __init__(self, ticker, price, in_money, ex_date=None, div=None):
         self.ticker = ticker
-        self.price = price
-        self.ex_date = ex_date
+        self.price = price        
         self.div = div
         self.in_money = in_money
+        self.ex_date = ex_date
+        if self.ex_date:
+            self.ex_date = datetime.datetime.strptime(self.ex_date, '%d/%m/%Y')
         Stock_Trace._stock_dict[self.ticker] = self
         
     def div_return(self):
         return self.div/self.price
     
     def days_left(self):
-        if self.ex_date:
-            today = datetime.date.today()
+        if self.ex_date:            
+            today = datetime.datetime.today()            
             if self.ex_date > today:
                 return (self.ex_date - today).days
             else:
@@ -53,13 +55,15 @@ Stock_Trace('HZN.AX', 0.15, False)
 Stock_Trace('WAM.AX', 1.50, False)
 Stock_Trace('NCM.AX', 22.5, False)
 Stock_Trace('KSC.AX', 2.0, False)
-Stock_Trace('NHC.AX', 6.18, False, datetime.date(2023, 10, 23), 0.30)
-Stock_Trace('BOQ.AX', 5.35, False)
-Stock_Trace('ASG.AX', 2.30, False)
-Stock_Trace('CVL.AX', 0.88, False)
 Stock_Trace('STW.AX', 66.0, False)
 Stock_Trace('SLF.AX', 66.0, False)
 Stock_Trace('SFY.AX', 65.0, False)
+
+Stock_Trace('NHC.AX', 6.18, False, "23/10/2023", 0.3)
+Stock_Trace('ASG.AX', 2.3, False, "31/10/2023", 0.1)
+Stock_Trace('BOQ.AX', 5.35, False, "26/10/2023", 0.21)
+Stock_Trace('CVL.AX', 0.88, False, "29/11/2023", 0.03)
+Stock_Trace('ACF.AX', 0.80, False, "26/10/2023", 0.027)
 
 #in money
 Stock_Trace('AGL.AX', 16.8, True)
@@ -67,21 +71,13 @@ Stock_Trace('FMG.AX', 23.52, True)
 Stock_Trace('ASH.AX', 0.68, True)
 Stock_Trace('TLS.AX', 4.12, True)
 Stock_Trace('TCL.AX', 12.8, True)
-Stock_Trace('WLE.AX', 1.47, True, datetime.date(2023, 10, 17), 0.045)
+Stock_Trace('WLE.AX', 1.47, True, "17/10/2023", 0.045)
 Stock_Trace('ANZ.AX', 27.06, True)
 Stock_Trace('WBC.AX', 23.05, True)
-
-print(Stock_Trace._stock_dict)
-
-for key, value in Stock_Trace._stock_dict.items():
-    print(f"{key}: {value.price}")
-
 
 #todo: incorporate ex div dates into dictionary so stocks come up on ticker within +-1month of current date
 
 stock_symbols = [key for key in Stock_Trace._stock_dict.keys()]
-
-in_money = ['FMG.AX', 'TLS.AX', 'ASH.AX', 'TCL.AX', 'ANZ.AX', 'WBC.AX', 'WLE.AX']
 
 # Trading hours in 24-hour format (10 am to 4 pm)
 now = datetime.datetime.now()
@@ -127,18 +123,26 @@ def animate(i):
                 pass
             
             normalised_price = historical_data[stock_symbol][-i]/Stock_Trace._stock_dict[stock_symbol].price
+            buy_price = Stock_Trace._stock_dict[stock_symbol].price
+            current_price = historical_data[stock_symbol][-i]
+            days_left = Stock_Trace._stock_dict[stock_symbol].days_left()
+            
+            if Stock_Trace._stock_dict[stock_symbol].div:
+                div_yield = Stock_Trace._stock_dict[stock_symbol].div/current_price*100
+            else:
+                div_yield = 0
             
             if normalised_price > low_cutoff_ratio and normalised_price < high_cutoff_ratio:
                 
                 if Stock_Trace._stock_dict[stock_symbol].in_money:
                     plt.plot(range(normalized_data.index.size),
                              normalized_data[stock_symbol],
-                             label=f"{stock_symbol} (buy:${Stock_Trace._stock_dict[stock_symbol].price:.2f})(cur:${historical_data[stock_symbol][-i]:.2f})",
+                             label=f"{stock_symbol[0:3]} \${buy_price:.2f}({current_price:.2f}) div: {div_yield:.2f}% ex:{days_left} ",
                              marker='x')
                 else:
                     plt.plot(range(normalized_data.index.size),
                              normalized_data[stock_symbol],
-                             label=f"{stock_symbol} (buy:${Stock_Trace._stock_dict[stock_symbol].price:.2f})(cur:${historical_data[stock_symbol][-i]:.2f})",
+                             label=f"{stock_symbol[0:3]} \${buy_price:.2f}({current_price:.2f}) div: {div_yield:.2f}% ex:{days_left} ",
                              marker='o')
 
                 plt.xlabel("Time")
