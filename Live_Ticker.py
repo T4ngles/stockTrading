6ae6da80133d4ba753f8036b56965573fd11c0f0
T4ngles <rlau06@gmail.com>
2023-10-19 15:04:21 +1100

    Live Ticker - add Stock_Trace class for more functionailty and better readability

--- Live_Ticker.py
+++ Live_Ticker.py
@@ -6,52 +6,82 @@ import numpy
 import datetime
 import time
 
-# Define the ASX stock symbols and their respective normalization prices
-#stock_symbols = ["AFI.AX", "CRN.AX", "FMG.AX", "MIR.AX", "NEC.AX", "TLS.AX"] #change to dictionary with stock symbols and purchase price
-#normalization_prices = [7.0, 1.72, 23.52, 2.875, 2.01, 4.2]
 low_cutoff_ratio = 0.95
 high_cutoff_ratio = 1.05
 
-#stock_symbols = ['CBA.AX', 'FMG.AX', 'GNE.AX', 'VSL.AX', 'TLS.AX', 'HVN.AX', 'ASH.AX', 'JCS.AX', 'AFI.AX']
-
-purchase_prices = {}
-
-#to buy
-#purchase_prices['TPG.AX'] = 5.1 #
-#purchase_prices['ANZ.AX'] = 23 #
-#purchase_prices['WBC.AX'] = 18 #
-#purchase_prices['CBA.AX'] = 98 #4%!
-#purchase_prices['STO.AX'] = 7.5 #4.19%!
-#purchase_prices['QBE.AX'] = 15.2 #2.46%
-#purchase_prices['AFI.AX'] = 7.15 #3.47%
-#purchase_prices['EZL.AX'] =  1.02
-#purchase_prices['KOV.AX'] =  8.0
-#purchase_prices['GNE.AX'] =  2.1
-#purchase_prices['ALX.AX'] =  5.85 #23 Sep!
-#purchase_prices['VSL.AX'] =  7.5 #27 Sep
-
-purchase_prices['MFF.AX'] =  2.98 #6 Oct!
-purchase_prices['HVN.AX'] =  3.6 #13 Oct!
-purchase_prices['WAM.AX'] =  1.63 #17 Oct
-purchase_prices['NHC.AX'] =  6.0 #5.6 #23 Oct!
-#purchase_prices['BOQ.AX'] =  5.6 #Oct!
-
-
-
-#purchase_prices['STW.AX'] = 66 #3.47%
-#purchase_prices['SLF.AX'] = 66 #3.47%
-#purchase_prices['SFY.AX'] = 65 #3.47%
+class Stock_Trace:
+    _stock_dict = {}
+    
+    def __init__(self, ticker, price, in_money, ex_date=None, div=None):
+        self.ticker = ticker
+        self.price = price
+        self.ex_date = ex_date
+        self.div = div
+        self.in_money = in_money
+        Stock_Trace._stock_dict[self.ticker] = self
+        
+    def div_return(self):
+        return self.div/self.price
+    
+    def days_left(self):
+        if self.ex_date:
+            today = datetime.date.today()
+            if self.ex_date > today:
+                return (self.ex_date - today).days
+            else:
+                return None
+        else:
+            return None            
+        
+Stock_Trace('TPG.AX', 5.1, False)
+Stock_Trace('CBA.AX', 95.0, False)
+Stock_Trace('YAL.AX', 4.88, False)
+Stock_Trace('STO.AX', 7.2, False)
+Stock_Trace('QBE.AX', 15.2, False)
+Stock_Trace('AFI.AX', 7.15, False)
+Stock_Trace('EZL.AX', 1.02, False)
+Stock_Trace('KOV.AX', 8.0, False)
+Stock_Trace('GNE.AX', 2.1, False)
+Stock_Trace('ALX.AX', 5.85, False)
+Stock_Trace('VSL.AX', 7.5, False)
+Stock_Trace('NEC.AX', 1.95, False)
+Stock_Trace('MFF.AX', 2.98, False)
+Stock_Trace('HVN.AX', 3.6, False)
+Stock_Trace('FGX.AX', 1.07, False)
+Stock_Trace('UOS.AX', 0.52, False)
+Stock_Trace('HZN.AX', 0.15, False)
+Stock_Trace('WAM.AX', 1.50, False)
+Stock_Trace('NCM.AX', 22.5, False)
+Stock_Trace('KSC.AX', 2.0, False)
+Stock_Trace('NHC.AX', 6.18, False, datetime.date(2023, 10, 23), 0.30)
+Stock_Trace('BOQ.AX', 5.35, False)
+Stock_Trace('ASG.AX', 2.30, False)
+Stock_Trace('CVL.AX', 0.88, False)
+Stock_Trace('STW.AX', 66.0, False)
+Stock_Trace('SLF.AX', 66.0, False)
+Stock_Trace('SFY.AX', 65.0, False)
 
 #in money
-#purchase_prices['AGL.AX'] = 16.8
-purchase_prices['FMG.AX'] = 21.68 #10,016: 425 old 23.52||| 980.59: 45 new 21.68
-#purchase_prices['ASH.AX'] =  0.68 #957
-purchase_prices['TLS.AX'] =  4.12 # 2884 4.12
-purchase_prices['TCL.AX'] =  12.6 #Dec 2,857.6 226 TCL at 1260
+Stock_Trace('AGL.AX', 16.8, True)
+Stock_Trace('FMG.AX', 23.52, True)
+Stock_Trace('ASH.AX', 0.68, True)
+Stock_Trace('TLS.AX', 4.12, True)
+Stock_Trace('TCL.AX', 12.8, True)
+Stock_Trace('WLE.AX', 1.47, True, datetime.date(2023, 10, 17), 0.045)
+Stock_Trace('ANZ.AX', 27.06, True)
+Stock_Trace('WBC.AX', 23.05, True)
 
-stock_symbols = [key for key in purchase_prices.keys()]
+print(Stock_Trace._stock_dict)
 
-in_money = ['FMG.AX', 'TLS.AX', 'ASH.AX', 'TCL.AX']
+for key, value in Stock_Trace._stock_dict.items():
+    print(f"{key}: {value.price}")
+
+
+#todo: incorporate ex div dates into dictionary so stocks come up on ticker within +-1month of current date
+
+stock_symbols = [key for key in Stock_Trace._stock_dict.keys()]
+
+in_money = ['FMG.AX', 'TLS.AX', 'ASH.AX', 'TCL.AX', 'ANZ.AX', 'WBC.AX', 'WLE.AX']
 
 # Trading hours in 24-hour format (10 am to 4 pm)
 now = datetime.datetime.now()
@@ -60,49 +90,62 @@ trading_end = datetime.time(16, 0)
 
 fig = plt.figure()
 
+index_error = []
+
 def get_normalized_prices(stock_symbols):
-    ani = animation.FuncAnimation(fig,animate,interval=60000,cache_frame_data=False)
+    ani = animation.FuncAnimation(fig,animate,interval=30000,cache_frame_data=False)
     plt.show()    
 
 def animate(i):
     # Fetch historical data for the last 72 hours
-    historical_data = yf.download(stock_symbols, period="1d", interval="1m")["Close"]
+    historical_data = yf.download(stock_symbols, period="2d", interval="1m")["Close"]
+    
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
 
     # Normalize prices to the given normalization prices
-    normalization_prices = [purchase_prices[x] for x in list(historical_data)] #account for order that yf downloads data
+    normalization_prices = [ Stock_Trace._stock_dict[x].price for x in list(historical_data)] #account for order that yf downloads data
 
     normalized_data = historical_data / normalization_prices
     
     for stock_symbol in stock_symbols:
-        print(stock_symbol,":",round(historical_data[stock_symbol][-1],2)," purchase:",purchase_prices[stock_symbol]," norm:",round(normalized_data[stock_symbol][-1],2))
+        print(stock_symbol,":",round(historical_data[stock_symbol][-1],2)," purchase:", Stock_Trace._stock_dict[stock_symbol].price," norm:",round(normalized_data[stock_symbol][-1],2))
     plt.cla()
 
     # Plot the normalized prices
     for stock_symbol in stock_symbols:
-        #range(normalized_data.index.size) gives only the count of the index instead of the time
 
-        #get rid of nans if no trades in the past minutes
-        i = 1
-        while numpy.isnan(historical_data[stock_symbol][-i]):
-            i += 1
+        if Stock_Trace._stock_dict[stock_symbol].days_left() or Stock_Trace._stock_dict[stock_symbol].in_money == True:
+            #range(normalized_data.index.size) gives only the count of the index instead of the time       
+            i = 1
+
+            try:
+                while numpy.isnan(historical_data[stock_symbol][-i]):  #get rid of nans if no trades in the past minutes
+                    i += 1
+            except IndexError:
+                print(f"Index Error with {stock_symbol}")
+                i = -1 * len(historical_data[stock_symbol])
+                pass
             
-        if stock_symbol in in_money:
-            plt.plot(range(normalized_data.index.size),
-                     normalized_data[stock_symbol],
-                     label=f"{stock_symbol} (buy:${purchase_prices[stock_symbol]:.2f})(cur:${historical_data[stock_symbol][-i]:.2f})",
-                     marker='x')
-        else:
-            plt.plot(range(normalized_data.index.size),
-                     normalized_data[stock_symbol],
-                     label=f"{stock_symbol} (buy:${purchase_prices[stock_symbol]:.2f})(cur:${historical_data[stock_symbol][-i]:.2f})",
-                     marker='o')
+            normalised_price = historical_data[stock_symbol][-i]/Stock_Trace._stock_dict[stock_symbol].price
+            
+            if normalised_price > low_cutoff_ratio and normalised_price < high_cutoff_ratio:
+                
+                if Stock_Trace._stock_dict[stock_symbol].in_money:
+                    plt.plot(range(normalized_data.index.size),
+                             normalized_data[stock_symbol],
+                             label=f"{stock_symbol} (buy:${Stock_Trace._stock_dict[stock_symbol].price:.2f})(cur:${historical_data[stock_symbol][-i]:.2f})",
+                             marker='x')
+                else:
+                    plt.plot(range(normalized_data.index.size),
+                             normalized_data[stock_symbol],
+                             label=f"{stock_symbol} (buy:${Stock_Trace._stock_dict[stock_symbol].price:.2f})(cur:${historical_data[stock_symbol][-i]:.2f})",
+                             marker='o')
 
-        plt.xlabel("Time")
-        plt.ylabel("Normalized Price")
-        plt.title(f"Normalized Prices for ASX Stocks(As of {current_time})")
-        plt.legend(loc='upper left')
-        plt.grid()
+                plt.xlabel("Time")
+                plt.ylabel("Normalized Price")
+                plt.title(f"Normalized Prices for ASX Stocks(As of {current_time})")
+                plt.legend(loc='upper left')
+                plt.grid()
     plt.axhline(y=1, color='black', linestyle='--', label='Purhcase')    
     plt.axhline(y=1.01, color='red', linestyle='--', label='Target1')
     plt.axhline(y=1.02, color='green', linestyle='--', label='Target2')
